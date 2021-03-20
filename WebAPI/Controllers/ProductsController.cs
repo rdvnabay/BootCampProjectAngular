44141f137c76cb43b1377a8e30a0b371b1efaee0
Rıdvan Abay <ridvanabay@hotmail.com>
2021-03-20 15:55:26 +0300

    #1 add- Autofac DependecyResolvers

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+
+        }
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var data=_productService.GetAll();
+            return Ok(data);
+        }
+    }
+}
